cb8b96c239ba61d7e8664469038d14b812c62292
Josh Steward <stewardjosh10@gmail.com>
2021-02-28 11:00:09 +1100

    second commit

--- server.js
+++ server.js
@@ -1,19 +1,50 @@
 //Dependencies found here
+const { listenerCount } = require("events");
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-const cTable = require("console.table");
-const db = require(".");
+//const cTable = require("console.table");
+const assets = require("./assets");
 
 const connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
-  port: 8889,
+  // port
+  port: 3306,
 
   // Your username
   user: "root",
 
   // Your password
-  password: "root",
-  database: "ee_info_db"
+  password: "yes",
+  database: "tracker_db"
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+
+    questions();
+}); 
+
+function questions() {
+
+    inquirer
+        .prompt({
+            name: "choices ",
+            type:"list",
+            message:"What would you like to do?",
+            choices:[
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "View Departments",
+                "View Roles",
+                "View Employees",
+                "Update Employee Roles"
+            ]
+        })
+        .then(function())
+}
+
+
+
